f22829cdd92f08f371f71044592164b82f9fa835
linekdr <line.kouder@supinfo.com>
2025-01-14 18:47:26 +0100

    message

---
+++ javascript/index.js
@@ -0,0 +1,9 @@
+const API_KEY = '9a2f31c4';
+const BASE_URL = `https://www.omdbapi.com/?apikey=${API_KEY}`;
+
+
+const moviesContainer = document.getElementById('movies-container');
+const loadMoreButton = document.getElementById('load-more');
+
+
+getMovies 
