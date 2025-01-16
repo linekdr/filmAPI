7da174d8d5bfbb3f8eb64414e90896477fc32116
linekdr <line.kouder@supinfo.com>
2025-01-16 20:11:55 +0100

    Ajout de la fonctionnalité recherche

---
+++ javascript/search.js
@@ -0,0 +1,22 @@
+import { getMovies } from "./util.js";
+ 
+const search = document.getElementById("search");
+const moviesContainer = document.getElementById("movies-container");
+ 
+ 
+search.addEventListener('input', async () => {
+ 
+    moviesContainer.innerHTML = '';
+    const valeur = search.value;
+ 
+    if (valeur) {
+        const movies = await getMovies(valeur, 1);
+ 
+        movies.forEach(movie => {
+            const movieDiv = document.createElement('div');
+            movieDiv.innerHTML =  `<img src="${movie.Poster}" style="width: 50px; height: 50px;"> <br /> Titre:  ${movie.Title}`;
+   
+            moviesContainer.appendChild(movieDiv);
+        });
+    }
+});
