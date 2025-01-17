c16b5621d68d71fc804dece9703fa5fac9b0c56b
linekdr <line.kouder@supinfo.com>
2025-01-17 17:10:16 +0100

    fonctionnalité pour en savoir plus ajoutée

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,24 @@
-const API_KEY = '9a2f31c4';
-const BASE_URL = `https://www.omdbapi.com/?apikey=${API_KEY}`;
-
+import { getMovies } from './util.js';
 
 const moviesContainer = document.getElementById('movies-container');
 const loadMoreButton = document.getElementById('load-more');
+let page  = 1;
 
+async function displayMovies() {
+    console.log("Page :" + page)
+    const movies = await getMovies("2024", page);
+    
+    movies.forEach(movie => {
+        const movieDiv = document.createElement('div');
+        movieDiv.innerHTML =  `<img src="${movie.Poster}" style="width: 50px; height: 50px;"> <br /> Titre:  <a href="movie.html?id=${movie.imdbID}" >${movie.Title}</a>`;
 
-getMovies 
+        moviesContainer.appendChild(movieDiv);
+    });
+}
+
+loadMoreButton.addEventListener('click', () => {
+    page++;
+    displayMovies();
+});
+
+displayMovies();
